885cdd208751298e5779700ad96439be70e09041
pathu-221 <prathamaggarwal23@gmail.com>
2023-06-27 00:39:46 +0530

    added progress bar;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/Navbar'
 import { Toaster } from 'react-hot-toast'
+import NextNProgress, { NextNProgressProps } from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Navbar />
+    <NextNProgress />
     <Toaster position='top-center' />
     <Component {...pageProps} />
   </>
