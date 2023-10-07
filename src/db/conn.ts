3af7f214992c13c52c8b73b62f4b3461b319b0e8
pathu-221 <prathamaggarwal23@gmail.com>
2023-10-08 01:00:44 +0530

    change db based on environment

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -1,12 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 export async function connect() {
-    try {
-
-        const db = await mongoose.connect(process.env.ATLAS_URI);
-        if(db) console.log('connected to database')
-
-      } catch (err) {
-        console.error(err);
-      }
+	try {
+		const db = await mongoose.connect(process.env.ATLAS_URI);
+		if (process.env.NODE_ENV === "development") {
+			db.connection.useDb("test");
+		} else {
+			db.connection.useDb("staging");
+		}
+		if (db) console.log("connected to database");
+	} catch (err) {
+		console.error(err);
+	}
 }
